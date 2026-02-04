1faa45a3d32e4f3147fad7a24de74acc93e132e9
Sayantan Das <dassayantandas@gmail.com>
2026-02-04 13:17:05 +0530

    new

---
+++ twoSum3.js
@@ -0,0 +1,15 @@
+function twoSum(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while(left < right) {
+        let currSum = nums[left] + nums[right];
+        if(currSum === target) {
+            return [left, right];
+        } else if(currSum > target) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+}
+console.log(twoSum([2, 7, 11, 15], 9));
